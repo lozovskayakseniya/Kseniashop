6bcda34c0d33755ffee5cf752d152c059037c924
alicedova <alice.dova@gmail.com>
2018-01-29 20:09:01 +0700

    first
    first

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ksenia:8888/admin/');
+define('HTTP_CATALOG', 'http://ksenia:8888/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ksenia:8888/admin/');
+define('HTTPS_CATALOG', 'http://ksenia:8888/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/anton/Documents/Ksenia/admin/');
+define('DIR_SYSTEM', '/Users/anton/Documents/Ksenia/system/');
+define('DIR_IMAGE', '/Users/anton/Documents/Ksenia/image/');
+define('DIR_LANGUAGE', '/Users/anton/Documents/Ksenia/admin/language/');
+define('DIR_TEMPLATE', '/Users/anton/Documents/Ksenia/admin/view/template/');
+define('DIR_CONFIG', '/Users/anton/Documents/Ksenia/system/config/');
+define('DIR_CACHE', '/Users/anton/Documents/Ksenia/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/anton/Documents/Ksenia/system/storage/download/');
+define('DIR_LOGS', '/Users/anton/Documents/Ksenia/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/anton/Documents/Ksenia/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/anton/Documents/Ksenia/system/storage/upload/');
+define('DIR_CATALOG', '/Users/anton/Documents/Ksenia/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'Ksenia');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
